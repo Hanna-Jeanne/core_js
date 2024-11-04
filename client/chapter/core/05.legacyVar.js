24c168932635d9221c62683e2a5e2c5b67106620
Hanna-Jeanne <janghanna423@gmail.com>
2024-11-04 17:52:50 +0900

    - globalThis - legacyVar - dataType

--- client/chapter/core/05.legacyVar.js
+++ client/chapter/core/05.legacyVar.js
@@ -5,11 +5,13 @@
 // var로 선언한 변수의 스코프는 함수 스코프이거나 전역 스코프입니다.
 // 블록 기준으로 스코프가 생기지 않기 때문에 블록 밖에서 접근 가능합니다.
 
-// let outside = 'outer';
+function a() {
+  let outside = 'outer';
+}
 
-// {
-//   console.log(outside);
-// }
+a();
+
+// console.log(outside);
 
 // function a() {
 //   var outside = 'outer';
@@ -17,9 +19,7 @@
 
 // a();
 
-// {
 //   console.log(outside);
-// }
 
 // var는 변수의 중복 선언을 허용합니다
 
@@ -37,3 +37,7 @@ let b = 10;
 console.log(b);
 
 // 선언하기 전 사용할 수 있는 var
+
+console.log(c);
+
+let c = 123456;
--- client/chapter/core/06.dataType.js
+++ client/chapter/core/06.dataType.js
@@ -6,33 +6,72 @@
 
 // 1. 존재하지 않는(nothing) 값 / 비어있는(empty) 값 / 알 수 없는(unknown) 값
 let empty = null;
-console.log(typeof empty);
+console.log(empty);
 
 // 2. 값이 할당되지 않은 상태
 let undef;
-console.log(typeof undef);
+console.log(undef);
 
 // 3. 따옴표를 사용해 묶은 텍스트(큰", 작은', 역`)
-const double = 'hello';
+const double = 'hello'; // string literal
 const single = 'hello';
 const backtick = `인도의 ${double / 30} 이 맛있다.`;
 
 console.log(backtick);
 
+const str = new String('hello'); // string constructor function
+
 // console.log(typeof double, typeof single, typeof backtick);
 
 // 4. 정수, 부동 소수점 숫자(길이 제약)
+const integer = 150; // number literal
+const floatingPointNumber = 1.23;
+
+const num = new Number(23); // number constructor function
+
+console.log(floatingPointNumber);
+console.log(typeof integer);
 
 // 5. 길이에 제약이 없는 정수(예: 암호 관련 작업에서 사용)
+const bigInt = 123n;
+
+console.log(typeof bigInt);
+
 // 6. 참(true, yes) 또는 거짓(false, no)
+const isActive = true;
+console.log(typeof isActive);
+
+const bool = new Boolean(true);
+
 // 7. 데이터 컬렉션(collection) 또는 복잡한 엔티티(entity)
+const obj = {
+  name: 'tiger',
+  age: 42,
+};
+
+const obj_1 = new Object();
+
+console.log(typeof obj);
+
 // 8. 고유한 식별자(unique identifier)
+const uuid = Symbol('uuid');
+const uuid2 = Symbol('uuid');
+
+console.log(uuid === uuid2);
+
+console.clear();
 
 /* typeof 연산자의 2가지 사용법 ---------------------------------------------- */
 
 // 1) 연산자 typeof
 
 // 2) 함수 typeof()
+
+function a() {}
+
+a();
+
+new a();
 
 // 언어 상, 오류
 
@@ -53,11 +92,11 @@ console.log(backtick);
 
 const user = {
   name: 'tiger',
-  age: 42, // 객체 안에 키랑 밸류를 프로퍼티라고 함
+  age: 42,
   payment: false,
   sayHi: function () {
     console.log(this);
-  }, // 객체 안에 함수를 매서드라고 함
+  },
   sayHi2: () => {
     console.log(this);
   },
@@ -71,8 +110,49 @@ const user = {
   },
 };
 
+function User() {
+  this.payment = false;
+  this.age = 25;
+}
+
+const _a = new User();
+const _a2 = new User();
+const _a3 = new User();
+const _a4 = new User();
+const _a5 = new User();
+const _a6 = new User();
+
 // Array
+const arr = ['a', { name: 'tiger' }, () => {}, 4, 5];
+
+console.log(arr);
+
+const newArray = new Array(10);
+
+console.log(newArray);
+
+console.clear();
 
 // function
+function sum(a, b) {
+  return a + b;
+}
+
+const result = sum(10, 5);
+
+console.log(result);
+
+function 붕어빵틀(재료) {
+  return `따끈하고 맛있는 ${재료} 맛 붕어빵입니다!`;
+}
+
+const 팥붕 = 붕어빵틀('팥');
+const 슈붕 = 붕어빵틀('슈크림');
+const 와붕 = 붕어빵틀('와사비');
+const 피붕 = 붕어빵틀('피자');
+
+const f = new Function('asdasdasd');
+
+console.log(f);
 
 // this
