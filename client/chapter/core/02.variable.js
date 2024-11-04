cdf7ba1836b4dfaa8d7050f06079110897803b3b
Hanna-Jeanne <janghanna423@gmail.com>
2024-11-04 17:22:53 +0900

    중간 점검

--- client/chapter/core/02.variable.js
+++ client/chapter/core/02.variable.js
@@ -2,7 +2,24 @@
 /* Variables          */
 /* ------------------ */
 
+console.log('variable');
+
+let admin;
+let name;
+
+name = 'John';
+admin = name;
+
+console.log(admin);
+
+const OUR_PLANET_NAME = 'EARTH';
+let currentOnlineUserName = 'TIGER';
+
+currentOnlineUserName = 'beom';
+
 /* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+/* variables ----------------------------------------------------------- */
 
 // - 갯수 별 상품 가격 계산하기
 let calcProductPriceQuantity;
@@ -32,6 +49,14 @@ let fortuneToday = 'so good';
 //   },
 // };
 
-/* variables ----------------------------------------------------------- */
-
 /* constant variables -------------------------------------------------- */
+
+// - 1년 기준 일(day)자 수
+
+const DAY_PER_YEAR = 365;
+
+// - 브랜드 접두사
+const BRAND_PREFIX = 'Stussy';
+
+// - 상품 정보
+const product_info = '....';
