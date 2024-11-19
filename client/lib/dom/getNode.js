0333e73a264aed170aeceee33aa4056e18bb81dd
Hanna-Jeanne <janghanna423@gmail.com>
2024-11-19 17:46:08 +0900

    - module 분리

--- client/lib/dom/getNode.js
+++ client/lib/dom/getNode.js
@@ -1,29 +1,17 @@
-// getNode
-// function getNode(node, context = document) {
-//   // if(typeof context === 'string')
-//   if (context.nodeType !== 9) {
-//     context = document.querySelector(context);
-//   }
 
-//   return context.querySelector(node);
-// }
 
-function getNode(node, context = document) {
-  if (context.nodeType !== 9) context = getNode(context);
-  return context.querySelector(node);
+export function getNode(node,context = document){
+  
+  if(context.nodeType !== 9) context = document.querySelector(context);
+  return context.querySelector(node)
+
 }
 
-// getNodes
-// function getNodes(node, context = document) {
-//   // if(typeof context === 'string')
-//   if (context.nodeType !== 9) {
-//     context = document.querySelector(context);
-//   }
 
-//   return context.querySelectorAll(node);
-// }
 
-function getNodes(node, context = document) {
-  if (context.nodeType !== 9) context = getNode(context);
-  return context.querySelectorAll(node);
+export function getNodes(node,context = document){
+  
+  if(context.nodeType !== 9) context = getNode(context);
+  return context.querySelectorAll(node)
 }
+
--- client/main.js
+++ client/main.js
@@ -1,7 +1,42 @@
-console.log('안녕 자바스크립트!');
+//
 
-console.log('navigator.language');
+// named export           =>  import { } from '..'
+// default export         =>  import ... from '..'
 
-const a = 'hello';
+// import { getNode as $, getNodes } from './lib/dom/getNode.js';
+// import { insertLast } from './lib/dom/insert.js';
 
-console.log(a);
+// import clearContents from "./lib/dom/clearContents.js";
+
+import {
+  getNode as $,
+  getNodes,
+  typeError,
+  insertLast,
+  clearContents,
+} from './lib/index.js';
+
+//
+// 1. input 선택하기
+// 2. input 이벤트 바인딩
+// 3. input의 value 값 가져오기
+// 4. 숫자 더하기
+// 5. result에 출력하기
+
+const first = $('#firstNumber');
+const second = $('#secondNumber');
+const result = $('.result');
+
+function handleInput(e) {
+  const firstValue = Number(first.value);
+  const secondValue = +second.value;
+  const total = firstValue + secondValue;
+
+  // result.textContent = '';
+  clearContents(result);
+
+  insertLast(result, total);
+}
+
+first.addEventListener('input', handleInput);
+second.addEventListener('input', handleInput);
