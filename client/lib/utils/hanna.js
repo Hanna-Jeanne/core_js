6a6aa69f6b1ebf39436551bf3dd99653df1ece3b
Hanna-Jeanne <janghanna423@gmail.com>
2024-11-26 18:13:30 +0900

    learn. web component - 강의 예제 - 상태에 따라 변하는 버튼 컴포넌트

--- client/lib/utils/hanna.js
+++ client/lib/utils/hanna.js
@@ -127,6 +127,6 @@ function getGeolocation() {
   });
 }
 
-const data = await getGeolocation();
+// const data = await getGeolocation();
 
 // console.log( data );
--- client/main.js
+++ client/main.js
@@ -1,39 +1,75 @@
-import { getNode, setStorage, getStorage, deleteStorage } from './lib/index.js';
+import { Button } from './components/Button/Button.js';
 
-const textField = getNode('#textField');
-const clearButton = getNode('button[data-name="clear"]');
+const app = document.getElementById('app');
 
-// 1. 인풋 이벤트 바인딩
-//     - 인풋(textarea) 태그 선택
-//     - addEventListener('input',handler)
-//     - handler 함수 안에서 값 가져오기 (this.value)
-
-// 2. 인풋 값을 로컬 스토리지에 저장(타이핑 하는 순간 순간마다)
-//     - setStorage(key,value)
-
-// 3. init 함수 안에서 로컬스토리지에 있는 값을 가져와 인풋의 value로 설정
-//     - getStorage
-//     - text.value = value
-
-// 4. 새로고침 => 데이터 유지
-
-// 5. clear 버튼 클릭시 데이터 제거 (로컬스토리지, 인풋값)
-
-function handleInput() {
-  const value = this.value;
-  setStorage('text', value);
+function defineElements() {
+  customElements.define('custom-button', Button);
 }
 
-function handleClear() {
-  textField.value = '';
-  deleteStorage('text');
-}
+defineElements();
 
-function init() {
-  getStorage('text').then((res) => (textField.value = res));
-}
+const buttonElement = document.createElement('custom-button');
+app.append(buttonElement);
 
-textField.addEventListener('input', handleInput);
-clearButton.addEventListener('click', handleClear);
+// 예제
+// class MyElement extends HTMLElement {
 
-init();
+//   connectedCallback(){
+
+//   constructor(){
+//     super();
+//     this.attachShadow({mode:'open'});
+//     this.count = 0;
+//   }
+
+//   connectedCallback(){
+//     this.랜더링();
+//     this.card = this.shadowRoot.querySelector('.card');
+//     this.card.addEventListener('click',()=>{
+//       this.이벤트()
+//     })
+//     this.card.addEventListener('click', this.이벤트.bind(this))
+//   }
+
+//   이벤트(){
+//     console.log(++this.count);
+//   }
+//   랜더링(){
+//     this.shadowRoot.innerHTML = `
+
+//       <style>
+//        @import url('./style.css');
+//         .card{
+//           background-color:orange;
+//         }
+//       </style>
+
+//       <div class="card">
+//         <slot name="title">Default Title</slot>
+//         <slot name="content">Default Contents</slot>
+//         <slot></slot>
+//         <h2>Card Title</h2>
+//         <slot name="me"></slot>
+//         <p> description..</p>
+//       </div>
+
+//     `
+//   }
+
+// }
+// customElements.define('my-element',MyElement);
+//   static get observedAttributes(){
+//     return ['data-name']
+//   }
+
+//   attributeChangedCallback(name,oldValue,newValue){
+//     if(name === 'data-name'){
+//       this.랜더링();
+//       console.log('리랜더링');
+
+//     }
+//   }
+
+// }
+
+// customElements.define('my-element',MyElement);
